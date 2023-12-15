5d5a3c9fd2347b7fe56e4182255719249ab65839
SharmaYashasvi <202011079@diu.iiitvadodara.ac.in>
2023-12-15 18:50:05 +0530

    Error Resolved

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import LoginSignUp from "./LoginSignUp";
-import Task from "./task";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginSignUp />} />
-        <Route path="/box" element={<Task />} />
       </Routes>
       <Toaster
         toastOptions={{
